f63d61fbb516c9d9865b74244fe528b9d41f5530
Lee Dongyun <notonalcyone@gmail.com>
2017-05-31 03:44:56 +0900

    update
    logic change

--- peria_item.py
+++ peria_item.py
@@ -6,19 +6,57 @@ class Item:
 		self.name = name
 		self.inputCast = []
 		self.outputCast = []
+		self.act = []
+		self.sync = False
 
-	def addInputCast(self,name,act,output = None):
-		self.inputCast.append(Cast(name,act,output))
+	def addInputCast(self,name,link = None):
+		self.inputCast.append(Cast(name,link))
+	def addOutputCast(self,name):
+		self.outputCast.append(Cast(name))
+	def addAct(self,act,link = None):
+		self.act.append(Act(act,link))
 
-
-	def addOutputCast(self,name,act):
-		self.outputCast.append(Cast(name,act))
-
-class Cast:
-	def __init__(self,name,act,output = None):
-		self.name = name
+class Act:
+	def __init__(self,act,link = None):
 		self.act = act
-		self.linkedCast = output
+		self.link = link
+class Cast:
+	def __init__(self,name,link = None):
+		self.name = name
+		self.link = link
+
+
+def starter(itemNumber,name):
+	print("하지마루용")
+	inputCheck(itemNumber,name)
+
+
+
+
+
+def inputCheck(itemNumber,name):
+	item = itemTable[itemNumber]
+	for i in range(0,len(item.inputCast)):
+		if item.inputCast[i].name == name:
+			if item.inputCast[i].link != None:
+				actCheck(itemNumber,item.inputCast[i].link )
+
+def actCheck(itemNumber,link):
+	item = itemTable[itemNumber]
+	print(item.act[link].act)
+	if item.act[link].link != None:
+		outputCheck(itemNumber,link)
+
+def outputCheck(itemNumber,link):
+	item = itemTable[itemNumber]
+	for i in range(0,len(itemTable)):
+		for j in range(0,len(itemTable[i].inputCast)):
+			if itemTable[i].inputCast[j].name == item.outputCast[link].name:
+				inputCheck(i,itemTable[i].inputCast[j].name)
+
+
+
+"""
 
 def itemMix(itemNumber,name):
 	item = itemTable[itemNumber]
@@ -36,24 +74,23 @@ def outputToInput(name):
 	for i in range(0,len(itemTable)):
 		for j in range(0,len(itemTable[i].inputCast)):
 			if itemTable[i].inputCast[j].name == name:
-				print(itemTable[i].inputCast[j].act)
+				itemMix(i,itemTable[i].inputCast[j].name)
 
 
-	"""
 	for i in range(0,len(itemTable)):
 		for j in range(0,len(itemTable[i].inputCast))
-	"""
+
 
 
 def finalOutput(name):
 	print(name)
 
 
-	"""
+
 	search = itemData[itemNumber].inputCast
 	for i in range(0,len(search)):
 		if cast == search[i].name:
 			print(search[i].act)
-	"""
 
+"""
 
--- script.py
+++ script.py
@@ -3,20 +3,33 @@ from peria_item import *
 
 
 tmpItem = Item("전등")
-tmpItem.addInputCast("불을 켜라","Activate Light")
-tmpItem.addInputCast("불을 꺼라","Deactivate Light")
+tmpItem.addInputCast("불을 켜라",0)
+tmpItem.addInputCast("불을 꺼라",1)
+tmpItem.addAct("Activate Light")
+tmpItem.addAct("Deactivate Light")
 itemData.append(tmpItem)
 
+tmpItem = Item("전등")
+tmpItem.addInputCast("불을 켜라",0)
+tmpItem.addInputCast("불을 꺼라",1)
+tmpItem.addAct("Activate Light")
+tmpItem.addAct("Deactivate Light")
+itemData.append(tmpItem)
+
+
 tmpItem = Item("스위치")
-tmpItem.addInputCast("스위치를 켜라","Switch ON",0)
-tmpItem.addInputCast("스위치를 꺼라","Switch OFF",1)
-tmpItem.addOutputCast("불을 켜라","Switch ON")
-tmpItem.addOutputCast("불을 꺼라","Switch OFF")
+tmpItem.addInputCast("스위치를 켜라",0)
+tmpItem.addInputCast("스위치를 꺼라",1)
+tmpItem.addAct("Switch On",0)
+tmpItem.addAct("Switch OFF",1)
+tmpItem.addOutputCast("불을 켜라")
+tmpItem.addOutputCast("불을 꺼라")
 itemData.append(tmpItem)
 
 
 itemTable.append(itemData[0])
 itemTable.append(itemData[1])
+itemTable.append(itemData[2])
 
 
-itemMix(1,"스위치를 꺼라")
+starter(2,"스위치를 켜라")
